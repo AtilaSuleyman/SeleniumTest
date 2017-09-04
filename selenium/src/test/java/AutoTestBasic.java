56c72c8da9048dc19e9337962aa0b1737a30eb4e
Atila Suleyman <atilasuleyman@hotmail.co.uk>
2017-09-04 14:38:31 +0000

    test goes on qa and checks if it is on the page expected

--- selenium/src/test/java/AutoTestBasic.java
+++ selenium/src/test/java/AutoTestBasic.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by Administrator on 04/09/2017.
  */
@@ -25,6 +27,8 @@ public class AutoTestBasic {
     @Test
     public void testOne(){
         driver.navigate().to("http://www.qa.com");
+        System.out.println(driver.getTitle());
+        assertEquals(driver.getTitle(), "IT Training | Project Management Training | Business Skills Training | QA");
     }
 
 }
--- selenium/src/test/java/Basic.java
+++ selenium/src/test/java/Basic.java
@@ -1,17 +1,19 @@
 import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
 
 /**
  * Created by Administrator on 04/09/2017.
  */
 public class Basic {
 
-    @Test
-    public void myTestMethod(){
-        System.out.println("Hello World");
+//    @Test
+//    public void myTestMethod(){
+//        System.out.println("Hello World");
+//    }
+
+    @BeforeClass
+    public static void onlyOnce(){
+        System.out.println("Before Class");
     }
 
     @Before
@@ -25,7 +27,7 @@ public class Basic {
     }
 
     @AfterClass
-    public static void myAfterClass(){
+    public static void myAfterClass() {
         System.out.println("After Class");
     }
 
